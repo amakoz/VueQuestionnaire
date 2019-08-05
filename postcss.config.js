f3f1763c6c81390f18ed26ecba167933deb20bc3
amadeuszkozlowski <amadeuszkozlowski@gmail.com>
2019-08-05 18:46:50 +0200

    [feature] import fonts and styles

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: {
+    'postcss-preset-env': { stage: 0 },
     tailwindcss: {},
     'vue-cli-plugin-tailwind/purgecss': {},
-    autoprefixer: {}
+    autoprefixer: {},
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 import './registerServiceWorker';
-import './assets/app.scss';
+import './assets/styles/app.scss';
 
 Vue.config.productionTip = false;
 
